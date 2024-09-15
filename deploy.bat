c71f94cfd28cc20d73cc580f3481999b6c1b49f3
Leelee <Leelee751220@gamil.com>
2024-09-15 15:14:08 +0800

    init deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,19 @@
-scp dist.zip root@47.236.249.79:/www/server/frogooo.com/dist.zip 
+@echo off
+cls 
+
+set service=frogooo.com
+set svcname=dist.tar.gz
+
+cd /d %~dp0
+
+call npm run build
+cd dist
+call tar -czvf %~dp0%svcname% *.* 
+
+plink -batch -pw %psd% %user%@%host% "mkdir -p %root%/%service%"
+pscp -batch -pw %psd% %~dp0%svcname% %user%@%host%:%root%/%service%/%svcname%
+plink -batch -pw %psd% %user%@%host% "tar -xzvf %root%/%service%/%svcname% -C %root%/%service%"
+plink -batch -pw %psd% %user%@%host% "rm -rf %root%/%service%/%svcname%"
+del %~dp0%svcname%
+
+
